f52f1010c8ab370c6411119dc92bbe997f0ef304
Florian Bauer <florian@fsrv.xyz>
2023-05-22 11:28:19 +0000

    squash: patch-f87edcff-d1ca-4466-97f1-96af316fb1bd
    Squashed commit of the following:
    
    * feat: add further search functions; parse times
    
    See merge request https://ref.ci/fsrvcorp/libraries/distroinfo/-/merge_requests/8

--- read.go
+++ read.go
@@ -2,12 +2,24 @@ package distroinfo
 
 import (
 	"errors"
+	"sort"
 	"strings"
+	"time"
 )
 
 var (
 	DistroFamilyNotFoundError  = errors.New("distro family not found")
 	DistroVersionNotFoundError = errors.New("distro version not found")
+)
+
+type SupportPeriod int
+
+const (
+	Unsupported SupportPeriod = iota
+	Unreleased
+	StandardSupport
+	LongTermSupport
+	ExtendedLongTermSupport
 )
 
 func GetDistroInfoByVersion(distro string, version string) (*DistroInfo, error) {
@@ -22,4 +34,88 @@ func GetDistroInfoByVersion(distro string, version string) (*DistroInfo, error) 
 		}
 	}
 	return nil, DistroVersionNotFoundError
+}
+
+func GetDistros() []string {
+	var distributions []string
+	for distro := range distroInfoStore {
+		distributions = append(distributions, distro)
+	}
+	sort.Strings(distributions)
+	return distributions
+}
+
+func GetDistroVersions(distro string) ([]string, error) {
+	family, familyFound := distroInfoStore[strings.ToLower(distro)]
+	if !familyFound {
+		return nil, DistroFamilyNotFoundError
+	}
+
+	var versions []string
+	for _, distroInfoEntry := range family {
+		versions = append(versions, distroInfoEntry.Version)
+	}
+	return versions, nil
+}
+
+func GetSupportedDistroVersions(distro string) (map[string]SupportPeriod, error) {
+	family, familyFound := distroInfoStore[strings.ToLower(distro)]
+	if !familyFound {
+		return nil, DistroFamilyNotFoundError
+	}
+
+	versions := make(map[string]SupportPeriod)
+
+	for _, distroInfoEntry := range family {
+		period := getSupportPeriod(distroInfoEntry)
+		if period == Unsupported {
+			continue
+		}
+		if period == Unreleased {
+			continue
+		}
+
+		versions[distroInfoEntry.Version] = period
+	}
+	return versions, nil
+}
+
+func getSupportPeriod(distroInfoEntry DistroInfo) SupportPeriod {
+	var isLts bool
+	var isELts bool
+	var isStandard bool
+
+	if distroInfoEntry.Release != nil && distroInfoEntry.Release.After(time.Now()) {
+		return Unreleased
+	}
+
+	if distroInfoEntry.Eol != nil && distroInfoEntry.Eol.After(time.Now()) {
+		isStandard = true
+	}
+
+	if distroInfoEntry.EolLts != nil && distroInfoEntry.EolLts.After(time.Now()) {
+		isLts = true
+	}
+
+	if distroInfoEntry.EolEsm != nil && distroInfoEntry.EolEsm.After(time.Now()) {
+		isLts = true
+	}
+
+	if distroInfoEntry.EolElts != nil && distroInfoEntry.EolElts.After(time.Now()) {
+		isELts = true
+	}
+
+	if isStandard {
+		return StandardSupport
+	}
+
+	if isLts && !isStandard {
+		return LongTermSupport
+	}
+
+	if isELts && !isStandard && !isLts {
+		return ExtendedLongTermSupport
+	}
+
+	return Unsupported
 }
--- read_test.go
+++ read_test.go
@@ -3,7 +3,18 @@ package distroinfo
 import (
 	"reflect"
 	"testing"
+	"time"
 )
+
+func mustParseTime(layout string, value string) *TimeStamp {
+	result, err := time.Parse(layout, value)
+	if err != nil {
+		panic(err)
+	}
+	return &TimeStamp{result}
+}
+
+const timeFormat = "2006-01-02"
 
 func TestGetDistroInfoByVersion(t *testing.T) {
 	type args struct {
@@ -26,11 +37,29 @@ func TestGetDistroInfoByVersion(t *testing.T) {
 				Version:  "1.2",
 				Codename: "Rex",
 				Series:   "rex",
-				Created:  "1996-06-17",
-				Release:  "1996-12-12",
-				Eol:      "1998-06-05",
+				Created:  mustParseTime(timeFormat, "1996-06-17"),
+				Release:  mustParseTime(timeFormat, "1996-12-12"),
+				Eol:      mustParseTime(timeFormat, "1998-06-05"),
 			},
 			wantErr: false,
+		},
+		{
+			name: "non-existing version",
+			args: args{
+				distro:  "debian",
+				version: "non-existing",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "non-existing distro",
+			args: args{
+				distro:  "non-existing",
+				version: "1.2",
+			},
+			want:    nil,
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -43,6 +72,75 @@ func TestGetDistroInfoByVersion(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetDistroInfoByVersion() got = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestGetDistros(t *testing.T) {
+	tests := []struct {
+		name string
+		want []string
+	}{
+		{
+			name: "get distros",
+			want: []string{"debian", "ubuntu"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetDistros(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetDistros() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetDistroVersions(t *testing.T) {
+	tests := []struct {
+		name    string
+		distro  string
+		wantErr bool
+	}{
+		{
+			name:    "get debian versions",
+			distro:  "debian",
+			wantErr: false,
+		},
+		{
+			name:    "get ubuntu versions",
+			distro:  "ubuntu",
+			wantErr: false,
+		},
+		{
+			name:    "get non-existing distro versions",
+			distro:  "non-existing",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, err := GetDistroVersions(tt.distro); (err != nil) != tt.wantErr {
+				t.Errorf("GetDistroVersions() error = %v, wantErr %v", err, tt.wantErr)
+			} else if !tt.wantErr && len(got) == 0 {
+				t.Errorf("GetDistroVersions() got = %v, want %v", got, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetSupportedDistroVersions(t *testing.T) {
+	for _, distro := range GetDistros() {
+		t.Run(distro, func(t *testing.T) {
+			versions, err := GetSupportedDistroVersions(distro)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(versions) == 0 {
+				t.Error("No supported versions found")
+			}
+
+			t.Log(versions)
 		})
 	}
 }
